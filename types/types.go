e4185d76cc439194e175f096533e3443922b0ae3
sato <ysudqfs@163.com>
2023-04-02 20:03:06 +0800

    update

--- types/types.go
+++ types/types.go
@@ -1,4 +1,4 @@
-package handler
+package types
 
 type Role string
 
@@ -22,16 +22,41 @@ const (
 	Variant MessageActionType = "variant"
 )
 
+type RespCode int
+
+const (
+	Success   RespCode = 0
+	Failed    RespCode = 1
+	AuthError RespCode = 2
+)
+
 type BaseResp struct {
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
-	Status  string      `json:"status"`
+	Status  RespCode    `json:"status"`
 }
 
 type ChatRequest struct {
-	Options       string `json:"options"`
-	Prompt        string `json:"prompt"`
-	SystemMessage string `json:"systemMessage"`
+	Options       interface{} `json:"options"`
+	Prompt        string      `json:"prompt"`
+	SystemMessage string      `json:"systemMessage"`
+}
+type BaseChatMessage struct {
+	Id      string `json:"id"`
+	Object  string `json:"object"`
+	Created int    `json:"created"`
+	Model   string `json:"model"`
+	Choices []struct {
+		Text         string      `json:"text"`
+		Index        int         `json:"index"`
+		Logprobs     interface{} `json:"logprobs"`
+		FinishReason string      `json:"finish_reason"`
+	} `json:"choices"`
+	Usage struct {
+		PromptTokens     int `json:"prompt_tokens"`
+		CompletionTokens int `json:"completion_tokens"`
+		TotalTokens      int `json:"total_tokens"`
+	} `json:"usage"`
 }
 
 type ChatMessage struct {
@@ -71,4 +96,41 @@ type ChatConfig struct {
 	ReverseProxy string  `json:"reverseProxy"`
 	HttpsProxy   string  `json:"httpsProxy"`
 	SocksProxy   string  `json:"socksProxy"`
+}
+
+type ConfigResp struct {
+	TimeoutMs    int32  `json:"timeoutMs"`
+	ReverseProxy string `json:"reverseProxy"`
+	ApiModel     string `json:"apiModel"`
+	SocksProxy   string `json:"socksProxy"`
+	HttpsProxy   string `json:"httpsProxy"`
+	Valance      string `json:"balance"`
+}
+
+type SessionResp struct {
+	Auth  bool   `json:"auth"`
+	Model string `json:"model"`
+}
+
+type VerifyRequest struct {
+	Token string `json:"token"`
+}
+
+type Choice struct {
+	Delta        Delta  `json:"delta"`
+	Index        int32  `json:"index"`
+	FinishReason string `json:"finish_reason"`
+}
+
+type CreateChatCompletionDeltaResponse struct {
+	Id      string   `json:"id"`
+	Object  string   `json:"object"`
+	Created int32    `json:"created"`
+	Model   string   `json:"model"`
+	Choices []Choice `json:"choices"`
+}
+
+type Delta struct {
+	Role    Role   `json:"role"`
+	Content string `json:"content"`
 }
